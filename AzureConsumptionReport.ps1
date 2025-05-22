c40ff312aaaa906542ab21535187027ec259a90b
wshamroukh <wshamroukh@gmail.com>
2025-05-22 22:22:05 +0400

    StartDate is always First day of the current month unless today is the first day of the month, In which case it should be the first day of the previous month,

--- AzureConsumptionReport.ps1
+++ AzureConsumptionReport.ps1
@@ -73,7 +73,60 @@ function Get-EndDate {
 }
 
 do {
-    $startDate = Show-DatePicker -Title "Start Date Selection" -Message "Please select the start date"
+    # Determine default start date based on today's date
+    $today = [datetime]::Today
+    if ($today.Day -eq 1) {
+        $defaultStartDate = Get-Date -Year $today.AddMonths(-1).Year -Month $today.AddMonths(-1).Month -Day 1
+    } else {
+        $defaultStartDate = Get-Date -Year $today.Year -Month $today.Month -Day 1
+    }
+
+    # Show date picker with default start date
+    function Show-DatePickerWithDefault {
+        param (
+            [datetime]$defaultDate,
+            [string]$Title = "Select a Date",
+            [string]$Message = "Choose a date:"
+        )
+
+        $form = New-Object Windows.Forms.Form
+        $form.Text = $Title
+        $form.Width = 280
+        $form.Height = 180
+        $form.StartPosition = "CenterScreen"
+
+        $label = New-Object Windows.Forms.Label
+        $label.Text = $Message
+        $label.AutoSize = $true
+        $label.Location = New-Object Drawing.Point(15, 15)
+        $form.Controls.Add($label)
+
+        $datePicker = New-Object Windows.Forms.DateTimePicker
+        $datePicker.Format = 'Short'
+        $datePicker.Width = 220
+        $datePicker.Value = $defaultDate
+        $datePicker.MaxDate = [datetime]::Today
+        $datePicker.Location = New-Object Drawing.Point(15, 45)
+        $form.Controls.Add($datePicker)
+
+        $okButton = New-Object Windows.Forms.Button
+        $okButton.Text = "OK"
+        $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+        $okButton.Location = New-Object Drawing.Point(90, 85)
+        $form.AcceptButton = $okButton
+        $form.Controls.Add($okButton)
+
+        $dialogResult = $form.ShowDialog()
+        if ($dialogResult -eq [System.Windows.Forms.DialogResult]::OK) {
+            return $datePicker.Value.Date
+        } else {
+            Write-Host "Date selection was cancelled. Exiting..." -ForegroundColor Yellow
+            exit
+        }
+    }
+
+    # Use updated date picker with calculated default
+    $startDate = Show-DatePickerWithDefault -defaultDate $defaultStartDate -Title "Start Date Selection" -Message "Please select the start date"
     $endDate   = Show-DatePicker -Title "End Date Selection" -Message "Please select the end date"
 
     if ($startDate -ge $endDate) {
